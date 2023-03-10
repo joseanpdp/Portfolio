a803a16bae1e2e35f99eb7ae6250d81a01dc9631
jose <joseanperezdp@gmail.com>
2023-03-10 22:12:38 +0100

    Realización ejercicio 1 de JavaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Botones de contenido
+
 let secciones = document.querySelectorAll("section");
 let botonesDeContenido = document.querySelectorAll("#menu-contenidos > p");
 
@@ -48,3 +50,31 @@ botonesDeContenido[3].addEventListener("click", function(ev){
   botonesDeContenido[3].className = "boton-activado";
   nav.className = "invisible";
 });
+
+
+// Botones para seleccionar el tema
+
+let botonesDeTema = document.querySelectorAll("#menu-temas > p");
+
+botonesDeTema[0].addEventListener("click", function(ev){
+    document.body.className = "tema1";
+    botonesDeTema[0].className = "boton-activado";
+    botonesDeTema[1].className = "";
+    botonesDeTema[2].className = "";
+});
+
+
+botonesDeTema[1].addEventListener("click", function(ev){
+    document.body.className = "tema2";
+    botonesDeTema[0].className = "";
+    botonesDeTema[1].className = "boton-activado";
+    botonesDeTema[2].className = "";
+});
+
+botonesDeTema[2].addEventListener("click", function(ev){
+    document.body.className = "tema3";
+    botonesDeTema[0].className = "";
+    botonesDeTema[1].className = "";
+    botonesDeTema[2].className = "boton-activado";
+});
+
